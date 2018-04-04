90125e584716d2df7d96130169d18a9699bec06a
Aarsh Patil <patilaarsh@gmail.com>
2018-04-04 17:30:35 -0400

    Integrated the Animation Loading Gif File onto the page Load

--- frontend/js/buy.js
+++ frontend/js/buy.js
@@ -1,10 +1,39 @@
+var openModal = function() {
+        document.getElementById('modal').style.display = 'block';
+        document.getElementById('fade').style.display = 'block';
+};
+
+var closeModal = function() {
+        document.getElementById('modal').style.display = 'none';
+        document.getElementById('fade').style.display = 'none';
+};
+
+openModal();
+
 $(document).ready(function(){
+    
     //Load a HTTP Request GET for the data
     var request = new XMLHttpRequest();
     var url = "./assets/sample.json";
+    
+    var sleep = function() {
+        var x = 1;
+        var y = null; // To keep under proper scope
+
+        setTimeout(function() {
+            x = x * 3 + 2;
+            y = x / 2;
+        }, 9999999);
+    };
+    
+    for (j=0;j<99999;j=j+1) {
+        sleep();
+    }
+    
 
     request.onreadystatechange = function() {
         if (this.readyState == 4) {
+            closeModal();
             var data = JSON.parse(this.responseText);
             
             populateCards(data.products);
@@ -28,4 +57,6 @@ $(document).ready(function(){
             cardStatus[i].textContent = products[i].stat;
         }
     };
+    
+    
 });
